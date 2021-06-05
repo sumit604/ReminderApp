0a9cf5b9949b39f27488140e2b1fd986d225ef10
sumit604 <85385400+sumit604@users.noreply.github.com>
2021-06-05 12:47:32 +0530

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,16 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button3")
+                .setName("Enter")
+                .setLeft("24.38095238095238em")
+                .setTop("14.476190476190476em")
+                .setCaption("Enter")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
