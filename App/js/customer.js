492b346d6bb12729ca94345bab34ecefc314d3c7
sumit604 <85385400+sumit604@users.noreply.github.com>
2021-07-03 10:22:43 +0530

    Updated by CrossUI No-Code App Builder

--- App/js/customer.js
+++ App/js/customer.js
@@ -39,15 +39,63 @@ xui.Class('App.customer', 'xui.Module',{
             );
             
             host.ctl_layout5.append(
-                xui.create("xui.UI.Input")
-                .setHost(host,"xui_ui_input23")
-                .setLeft("21.333333333333332em")
-                .setTop("0.7619047619047619em")
-                .setWidth("18em")
-                .setHeight("7.695238095238095em")
-                .setLabelSize("8em")
-                .setLabelCaption("Customer Details")
-                .setMultiLines(true),
+                xui.create("xui.UI.FormLayout")
+                .setHost(host,"xui_ui_formlayout1")
+                .setLeft("11.428571428571429em")
+                .setTop("1.5238095238095237em")
+                .setWidth("33.82857142857143em")
+                .setHeight("31.085714285714285em")
+                .setLayoutData({
+                    "rows":5,
+                    "cols":2,
+                    "rowSetting":{
+                        "1":{
+                            "manualHeight":50
+                        },
+                        "2":{
+                            "manualHeight":50
+                        },
+                        "3":{
+                            "manualHeight":50
+                        },
+                        "4":{
+                            "manualHeight":50
+                        },
+                        "5":{
+                            "manualHeight":50
+                        }
+                    },
+                    "cells":{
+                        "A1":{
+                            "value":"Name"
+                        },
+                        "A2":{
+                            "value":"Contact No."
+                        },
+                        "A3":{
+                            "value":"Email Id"
+                        },
+                        "A4":{
+                            "value":"Event"
+                        },
+                        "A5":{
+                            "value":"Date"
+                        }
+                    }
+                }),
+                "main"
+            );
+            
+            host.ctl_layout5.append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"xui_ui_label1")
+                .setLeft("16em")
+                .setTop("3.8095238095238093em")
+                .setWidth("23.00952380952381em")
+                .setHeight("3.5047619047619047em")
+                .setCaption("    Customer Details")
+                .setFontSize("2em")
+                .setFontWeight("900"),
                 "before"
             );
             
